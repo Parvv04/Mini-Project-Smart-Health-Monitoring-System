7a363ab3726546ee336b5f0bcd234fe409679c9f
Parvv04 <parzzabeth12@gmail.com>
2025-10-31 05:31:20 +0000

    edited

--- data_logger.py
+++ data_logger.py
@@ -17,9 +17,35 @@ class DataLogger:
             with open(self.csv_path, "w", newline="") as f:
                 writer = csv.writer(f)
                 writer.writerow(["timestamp", "ear", "total_blinks", "blinks_last_min", "posture_angle", "alert"])
+        
+        self.firebase_ref = None
+        try:
+            # change the URL below to match your Firebase project URL
+            self.firebase_ref = init_firebase(
+                db_url="https://smarthealthmonitor-default-rtdb.asia-southeast1.firebasedatabase.app/"
+            )
+            print("✅ Connected to Firebase successfully")
+        except Exception as e:
+            print("⚠️ Firebase init failed:", e)
+
 
     def log(self, ear, total_blinks, blinks_last_min, posture_angle, alert):
         ts = datetime.now().isoformat()
         with open(self.csv_path, "a", newline="") as f:
             writer = csv.writer(f)
             writer.writerow([ts, ear if ear is not None else "", total_blinks, blinks_last_min, posture_angle if posture_angle is not None else "", alert if alert else ""])
+        # also push to Firebase
+        if self.firebase_ref:
+            payload = {
+        "timestamp": ts,
+        "ear": ear,
+        "total_blinks": total_blinks,
+        "blinks_last_min": blinks_last_min,
+        "posture_angle": posture_angle,
+        "alert": alert,
+        }
+        try:
+            push_log(self.firebase_ref, payload)
+        except Exception as e:
+            print("Firebase push failed:", e)
+     
